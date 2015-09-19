24c8cc248da7bd9f8d4bf9055f013042466e5cf7
rageshpayyan <rageshpayyan@gmail.com>
2015-09-19 09:39:55 -0700

    git class 3 test

---
+++ gitClass3/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitClass3
+{
+    public class Class1
+    {
+        public int fun1()
+        {
+            return 20;
+
+        }
+    }
+}
